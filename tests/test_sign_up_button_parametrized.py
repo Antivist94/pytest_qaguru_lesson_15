eb64025be308f87015eea542835f0048241e612b
Eugene Evdokimenko <evdokimenko@tochka.com>
2024-08-07 10:40:53 +0400

    updated tests 3 - 2

--- tests/test_sign_up_button_parametrized.py
+++ tests/test_sign_up_button_parametrized.py
@@ -1,22 +1,22 @@
 import pytest
 from selene import browser, be
 
-web_only = pytest.mark.parametrize("browser_manager_web", [(1920, 1080), (1280, 720), (1024, 768)],
+web_only = pytest.mark.parametrize("browser_manager", [(1920, 1080), (1280, 720), (1024, 768)],
                                    ids = ["WEB_width 1920", "WEB_width 1280", "WEB_width 1013"], indirect = True)
 
-mobile_only = pytest.mark.parametrize("browser_manager_mobile", [(320, 640), (360, 740), (412, 869)],
+mobile_only = pytest.mark.parametrize("browser_manager", [(320, 640), (360, 740), (412, 869)],
                                       ids = ["Mobile_640x320", "Mobile_740x360", "Mobile_869x412"], indirect = True)
 
 
 @web_only
-def test_sign_up_button_desktop(browser_manager_web):
+def test_sign_up_button_desktop(browser_manager):
     browser.open('')
     browser.element('.HeaderMenu-link--sign-up').click()
     browser.element('#email-container').should(be.visible)
 
 
 @mobile_only
-def test_sign_up_button_mobile(browser_manager_mobile):
+def test_sign_up_button_mobile(browser_manager):
     browser.open('')
     browser.element('.Button--link').click()
     browser.element('.HeaderMenu-link--sign-up').click()
